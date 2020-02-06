3dae0f2f7fae9bfc6cb88f3ed7f73cce7ad858e5
Kamil Jarmołowicz <kamiljarmol@protonmail.com>
2020-02-06 19:29:36 +0100

    Updates

---
+++ precache-manifest.bfaeb352727fe4430d197343b4257dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e6ec5ca0ccb901e1900",
+    "url": "/react-app/static/css/main.1b47bd55.chunk.css"
+  },
+  {
+    "revision": "8e6ec5ca0ccb901e1900",
+    "url": "/react-app/static/js/main.4c254a80.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/react-app/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "aad5aa4ce5ea5c0489092095de336339",
+    "url": "/react-app/index.html"
+  }
+];
